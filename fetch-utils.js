6ff174a81f76692e6fa4c7f54313a1448346e98c
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-17 15:58:35 -0800

    added export functions to fetch ustils.

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://nschbtdnjkjkoshunkhk.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zY2hidGRuamtqa29zaHVua2hrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ0MzYwMjAsImV4cCI6MTk2MDAxMjAyMH0.mwm25ysgqkbA7ZMQcYTguKcFC-qRnmiDHgK2PxCROPk';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -36,6 +36,30 @@ export async function logout() {
 
     return (window.location.href = '../');
 }
+
+export async function createList(list) {
+    const resp = await client.from('lists').insert({ list, complete: false, user_id: client.auth.user().id, })
+        .single();
+    return checkError(resp);
+}
+
+export async function deleteAllLists() {
+    const resp = await client.from('lists').delete({ user_id: client.auth.user().id, });
+    return checkError(resp);
+}
+
+export async function completeList(id) {
+    const resp = await client.from('lists').update({ complete: true }).match({user_id: client.auth.user().id, id: id });
+    return checkError(resp);
+}
+
+
+
+
+
+
+
+
 
 // function checkError({ data, error }) {
 //     return error ? console.error(error) : data;
