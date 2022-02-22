17363c02e905b120f0b55ad2b74578022e6113a8
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-21 17:18:14 -0800

    fixed test

--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -5,11 +5,11 @@ const test = QUnit.test;
 
 test('time to render item', (expect) => {
     
-    const expected = '<ul id="items"><div class="incomplete items"><p>spice</p></div><div class="complete items"><p>potatoes</p></div></ul>';
+    const expected = '<div class="incomplete items"><p>spice</p></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = (renderItem); 
+    const actual = renderItem({ complete: false, items:'spice' }); 
 
     
     expect.equal(actual.outerHTML, expected);
